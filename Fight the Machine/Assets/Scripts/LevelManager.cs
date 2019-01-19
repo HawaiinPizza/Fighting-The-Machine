dcb4b2aeeed4584ff4d0453fe5406fd85066e0b6
SimonAytes <44680601+SimonAytes@users.noreply.github.com>
2019-01-19 07:41:39 -0500

    Unity Uptade; 7:41 am

---
+++ Fight the Machine/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LevelSwitcher()
+    {
+        SceneManager.LoadScene(2); // Change the value of "2" to route to the desired scene. This can be monitored / assigned in the build settings.
+    }
+
+}
