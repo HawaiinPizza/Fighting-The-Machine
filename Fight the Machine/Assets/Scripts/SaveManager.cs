0c646f1684ddbd25f54dc069f17ea068543deab9
Kevin Brehm <kevcode4@gmail.com>
2019-01-19 21:04:18 -0500

    Save System

---
+++ Fight the Machine/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+
+    [Serializable]
+    public class SaveData
+    {
+        public AIPath[] path = new AIPath[Evolver.matchNum];
+        public metricAI[] metric = new metricAI[Evolver.matchNum];
+        public bool[] completed = new bool[Evolver.matchNum];
+    }
+
+    public class SaveManager
+    {
+        public static int matchesSinceEvo = 0;
+        public static SaveData saveData = new SaveData();
+
+        public static void LoadSaveData(int profile)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            string fileName = Path.Combine(Application.persistentDataPath, "save_data/profile" + profile + ".dat");
+            using (FileStream fileStream = File.Open(fileName, FileMode.Open))
+            {
+                saveData = (SaveData)binaryFormatter.Deserialize(fileStream);
+            }
+
+            Debug.Log(fileName);
+        }
+
+        public static void SaveSaveData(int profile)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            string fileName = Path.Combine(Application.persistentDataPath, "save_data/profile" + profile + ".dat");
+            using (FileStream fileStream = File.Open(fileName, FileMode.Open))
+            {
+                binaryFormatter.Serialize(fileStream, saveData);
+            }
+
+            Debug.Log(fileName);
+        }
+
+    }
+
+
+}
